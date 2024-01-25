835e1fe77276241496d2e600721f37b70b86f5ac
Kaspar Puhm <kaspar@klausapp.com>
2024-01-26 01:54:28 +0800

    feat: add proper validation errors

--- src/modules/interest-calculator/types/compound-interest-form.model.ts
+++ src/modules/interest-calculator/types/compound-interest-form.model.ts
@@ -4,10 +4,32 @@ export type CompoundInterestForm = z.infer<typeof CompoundInterestFormSchema>;
 
 const FrequencySchema = z.enum(["YEARLY", "MONTHLY"]);
 
+const TransformStringToNumber = z
+  .union([z.number(), z.string()])
+  .transform((val) => {
+    return typeof val === "number" ? val : undefined;
+  });
+
 export const CompoundInterestFormSchema = z.object({
-  initialInvestment: z.number(),
+  initialInvestment: TransformStringToNumber.pipe(
+    z.number({
+      required_error: "Initial investment is required, enter 0 to skip",
+    }),
+  ),
   contributionFrequency: FrequencySchema,
-  contributionAmount: z.number(),
-  years: z.number(),
-  estimatedReturn: z.number().min(1),
+  contributionAmount: TransformStringToNumber.pipe(
+    z.number({
+      required_error: "Contribution amount is required, enter 0 to skip",
+    }),
+  ),
+  years: TransformStringToNumber.pipe(
+    z
+      .number({ required_error: "Year is required" })
+      .min(1, "Minimal year count is 1"),
+  ),
+  estimatedReturn: TransformStringToNumber.pipe(
+    z.number({
+      required_error: "Estimated return is required, enter 0 to skip",
+    }),
+  ),
 });
